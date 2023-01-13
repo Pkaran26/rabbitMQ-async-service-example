f772e3b8bdad6ba09132f89ecd5631d48f4810d5
prateek <prateek.kaushik@indiqus.com>
2023-01-14 02:11:19 +0530

    minor

--- qhandler/rmq-server.js
+++ qhandler/rmq-server.js
@@ -24,6 +24,57 @@ const handleService = async (rabbitMQ, data, serviceQueue)=>{
   })
 }
 
+const apiMap = {
+  type: 'sync',
+  callMap: {
+    1: {
+      name: 'blog_posts',
+      next:[2]
+    },
+    2: {
+      name: 'blog_users',
+      next:[3]
+    },
+    3: {
+      name: 'product_products',
+      next:[]
+    }
+  }
+}
+
+const runApiMap = (rabbitMQ, data, )=>{
+  const {serviceType, payload} = JSON.parse(data.content)
+  return new Promise( async(resolve, reject)=>{
+    let index = 1
+    let final = {}
+    while (true) {
+    const res = apiMap.callMap[index].name.split('_')
+    
+    console.log(index,": ", res[0], res[1]);
+  
+    const result = await handleService(rabbitMQ, {
+      ...data, 
+      content: {
+        ...payload,
+        params: {
+          ...payload.params,
+          api_name: res[1]
+        }
+      }
+    }, res[0]+'Queue')
+
+    final[res[1]] = result
+  
+    if (apiMap.callMap[index].next.length > 0) {
+      index = apiMap.callMap[index].next[0]
+    } else {
+      resolve(final)
+      break
+    }
+    }
+  })
+}
+
 const rmqServer = async ()=>{
   const taskQueue = 'requestQueue'
   const serviceQueues = ['blogQueue', 'productQueue']
@@ -33,11 +84,9 @@ const rmqServer = async ()=>{
     await rabbitMQ.createServerQueue(taskQueue)
 
     rabbitMQ.consumeByServer(taskQueue, async (data)=>{
-      const {serviceType, payload} = JSON.parse(data.content)
-      if (serviceQueues.includes(serviceType+'Queue')) {
-        const result = await handleService(rabbitMQ, {...data, content: payload}, serviceType+'Queue')
-        rabbitMQ.sendToClient(JSON.stringify(result), data.replyTo, data.correlationId)
-
+      if (apiMap.callMap) {
+        let final = await runApiMap(rabbitMQ, data)
+        rabbitMQ.sendToClient(JSON.stringify(final), data.replyTo, data.correlationId)
       } else {
         rabbitMQ.sendToClient(JSON.stringify({success: false, message: "service does not matched"}), data.replyTo, data.correlationId)
       }
